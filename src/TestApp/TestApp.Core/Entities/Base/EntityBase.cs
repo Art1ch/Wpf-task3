fc5d88baa29a542d14052b47f387fa5406b21b20
Artem Bogdanovich <bogdanovicha898@gmail.com>
2026-07-08 19:44:34 +0300

    Setup models

---
+++ src/TestApp/TestApp.Core/Entities/Base/EntityBase.cs
@@ -0,0 +1,7 @@
+﻿namespace TestApp.Core.Entities.Base;
+
+public abstract class EntityBase 
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
